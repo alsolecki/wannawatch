c274502443ad007de371fc11243781ad416df445
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-10-17 22:43:25 -0400

    added CRUD functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,83 @@ import AddMovie from './components/AddMovie.jsx'
 import Content from './components/Content.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
-
+import apiRequest from './components/apiRequest.jsx'
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('movieList')) || []);
-  const [newMovie, setNewMovie] = useState('');
 
+  const API_URL = 'http://localhost:3500/movies';
+  const [items, setItems] = useState([]);
+  const [newMovie, setNewMovie] = useState('');
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    localStorage.setItem('movieList', JSON.stringify(items));
-  }, [items])
+    
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('did not receive expected data');
+        const listItems = await response.json();
+        console.log(listItems);
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    setTimeout(() => {
+      (async () => await fetchItems())();
+      }, 2000)
+
+    }, [])
+    
 
 
-  const addItem = (title) => {
+  const addItem = async (title) => {
     //ternary statement to set the id value
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = {id, checked: false, title}
     const listItems = [...items, myNewItem];
     setItems(listItems);
+
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+    const result = await apiRequest(API_URL, postOptions)
+    if (result) setFetchError(result); 
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
     setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ checked: myItem[0].checked})
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   };
 
-  const handleDelete = (id) => {
-    console.log(id);
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
+
+    const deleteOptions = { method: 'DELETE' };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
   }
 
   const handleSubmit = (e) => {
@@ -57,13 +105,15 @@ function App() {
           setNewMovie={setNewMovie}
           handleSubmit={handleSubmit}
         />
-
-        <Content
-          items={items}
-          handleCheck={handleCheck}
-          handleDelete={handleDelete}
-           />
-
+        <main>
+          {isLoading && <p>Loading Items...</p>}
+          {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+          {!fetchError && !isLoading && <Content
+            items={items}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />}
+        </main>
         <Footer
           length={items.length} />
         
